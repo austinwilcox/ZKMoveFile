c5f9cfdc9b8c7fa41173da8e553636fd18b90183
Austin Wilcox <awilcox@arbinger.com>
2023-10-10 17:00:44 -0600

    ready to merge into master

--- lua/Obsidian-nvim-move-file/init.lua
+++ lua/Obsidian-nvim-move-file/init.lua
@@ -1,4 +1,36 @@
+local popup = require('plenary.popup')
+local move_file_win_id = nil
+local move_file_bufnr = nil
+
 local M = {}
+
+local function create_window()
+    local width =  60
+    local height = 10
+    local borderchars =  { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+    local bufnr = vim.api.nvim_create_buf(false, false)
+
+    local obsidian_move_cmd_win_id, win = popup.create(bufnr, {
+        title = M.title or "ZK Directories",
+        highlight = "ObsidianWindow",
+        line = math.floor(((vim.o.lines - height) / 2) - 1),
+        col = math.floor((vim.o.columns - width) / 2),
+        minwidth = width,
+        minheight = height,
+        borderchars = borderchars,
+    })
+
+    vim.api.nvim_win_set_option(
+        win.border.win_id,
+        "winhl",
+        "Normal:ObsidianBorder"
+    )
+
+    return {
+        bufnr = bufnr,
+        win_id = obsidian_move_cmd_win_id,
+    }
+end
 
 function M.ObsidianMoveCurrentBuffer()
   local current_buf = vim.api.nvim_get_current_buf()
@@ -16,38 +48,48 @@ function M.ObsidianMoveCurrentBuffer()
     end
   end
 
-  local function showMenu()
-      print("Select Directory to move to:")
-      for i, option in ipairs(options) do
-          print(i .. ". " .. option)
-      end
+  local win_info = create_window()
+  move_file_bufnr = win_info.bufnr
+  move_file_win_id = win_info.win_id
 
-      while true do
-          local choice = vim.fn.input("Enter the number of your choice: ")
-          choice = tonumber(choice)
+  vim.api.nvim_win_set_option(move_file_win_id, "number", true)
+  vim.api.nvim_buf_set_name(move_file_bufnr, "obsidian-move-file-menu")
+  vim.api.nvim_buf_set_lines(move_file_bufnr, 0, #options, false, options)
+  vim.api.nvim_buf_set_option(move_file_bufnr, "filetype", "obsidian-move-file")
+  vim.api.nvim_buf_set_option(move_file_bufnr, "buftype", "acwrite")
+  vim.api.nvim_buf_set_option(move_file_bufnr, "bufhidden", "delete")
 
-          if choice and choice >= 1 and choice <= #options then
-              print("You selected: " .. options[choice])
-              print(full_path)
-              local filename = string.match(full_path, "[^/\\]+$")
-              local path_to_place_file = directory_path .. "/" .. options[choice] .. "/" .. filename
-              print(path_to_place_file)
-              local success, errorMsg = os.rename(full_path, directory_path .. "/" .. options[choice] .. "/" .. filename)
-              if success then
-                print("File moved successfully, reloading file in new buffer.")
-                vim.api.nvim_command("e " .. path_to_place_file)
-              else
-                print("Error moving file: " .. errorMsg)
-              end
-              return
-          else
-              print("Invalid choice. Please enter a valid option.")
-          end
-      end
+  vim.api.nvim_buf_set_keymap(
+      win_info.bufnr,
+      "n",
+      "<CR>",
+      "<Cmd>lua select_menu_item()<CR>",
+      {}
+  )
+
+  local function select_menu_item()
+    local choice = vim.fn.line(".")
+    local filename = string.match(full_path, "[^/\\]+$")
+    local path_to_place_file = directory_path .. "/" .. options[choice] .. "/" .. filename
+    local success, errorMsg = os.rename(full_path, directory_path .. "/" .. options[choice] .. "/" .. filename)
+    if success then
+      print("File moved successfully, reloading file in new buffer.")
+      vim.api.nvim_command("e " .. path_to_place_file)
+    else
+      print("Error moving file: " .. errorMsg)
+    end
+    close_menu()
   end
 
-  showMenu()
+  local function close_menu()
+    vim.api.nvim_win_close(move_file_win_id, true)
+
+    move_file_bufnr = nil
+    move_file_win_id = nil
+  end
+
 end
+
 
 local commands = {
   ObsidianMoveCurrentBuffer = { func = M.ObsidianMoveCurrentBuffer, opts = { nargs = 0} }
