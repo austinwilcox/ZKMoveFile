c7e5cea04ab5c23170f32470b0fcd2b7b9f99c8e
Austin Wilcox <awilcox@arbinger.com>
2023-10-10 16:56:06 -0600

    working! Plenary is now working the way I would like it to, and I can merge this code into master once it's moved into my init.lua

--- lua/Obsidian-nvim-move-file/test.lua
+++ lua/Obsidian-nvim-move-file/test.lua
@@ -1,6 +1,9 @@
 --NOTE: luafile test.lua is how you run this file
+--Or source %
 
 local popup = require("plenary.popup")
+local move_file_win_id = nil
+local move_file_bufnr = nil
 
 local function create_window()
     local width =  60
@@ -30,10 +33,34 @@ local function create_window()
     }
 end
 
-local contents = {"Test this out", "Test this out too"}
-win_info = create_window()
 
-vim.api.nvim_buf_set_lines(win_info.bufnr, 0, #contents, false, contents)
+local directory_path = '/home/austin/Zettelkasten-v2/'
+local files = vim.fn.readdir(directory_path)
+
+local options = { }
+for _, file in ipairs(files) do
+  --TODO: Make this a recursive function to get all subdirectories
+  if vim.fn.isdirectory(directory_path .. "/" .. file) == 1 then
+    if file:sub(1, 1) ~= "." then
+      table.insert(options, file)
+    end
+  end
+end
+
+local current_buf = vim.api.nvim_get_current_buf()
+local full_path = vim.api.nvim_buf_get_name(current_buf)
+local win_info = create_window()
+
+move_file_bufnr = win_info.bufnr
+move_file_win_id = win_info.win_id
+
+vim.api.nvim_win_set_option(move_file_win_id, "number", true)
+vim.api.nvim_buf_set_name(move_file_bufnr, "obsidian-move-file-menu")
+vim.api.nvim_buf_set_lines(move_file_bufnr, 0, #options, false, options)
+vim.api.nvim_buf_set_option(move_file_bufnr, "filetype", "obsidian-move-file")
+vim.api.nvim_buf_set_option(move_file_bufnr, "buftype", "acwrite")
+vim.api.nvim_buf_set_option(move_file_bufnr, "bufhidden", "delete")
+
 vim.api.nvim_buf_set_keymap(
     win_info.bufnr,
     "n",
@@ -43,23 +70,22 @@ vim.api.nvim_buf_set_keymap(
 )
 
 function select_menu_item()
-    -- log.trace("cmd-ui#select_menu_item()")
-    local cmd = vim.fn.line(".")
-  print(cmd)
-    -- close_menu(true)
-    -- local answer = vim.fn.input("Terminal index (default to 1): ")
-    -- if answer == "" then
-    --     answer = "1"
-    -- end
-    -- local idx = tonumber(answer)
-    -- if idx then
-    --     term.sendCommand(idx, cmd)
-    -- end
+  local choice = vim.fn.line(".")
+  local filename = string.match(full_path, "[^/\\]+$")
+  local path_to_place_file = directory_path .. "/" .. options[choice] .. "/" .. filename
+  -- local success, errorMsg = os.rename(full_path, directory_path .. "/" .. options[choice] .. "/" .. filename)
+  -- if success then
+  --   print("File moved successfully, reloading file in new buffer.")
+  --   vim.api.nvim_command("e " .. path_to_place_file)
+  -- else
+  --   print("Error moving file: " .. errorMsg)
+  -- end
+  close_menu()
 end
 
-function close_menu() 
-    vim.api.nvim_win_close(Harpoon_win_id, true)
+function close_menu()
+  vim.api.nvim_win_close(move_file_win_id, true)
 
-    Harpoon_win_id = nil
-    Harpoon_bufh = nil
+  move_file_bufnr = nil
+  move_file_win_id = nil
 end
